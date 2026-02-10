b6a38dd7210ec0db8cb99905c7e88d09f4cc84ea
AssuredPigeon <93557526+AssuredPigeon@users.noreply.github.com>
2026-02-10 02:54:12 -0800

    Registro y login en bd
    Removed the database password for security reasons.

--- backend/db.js
+++ backend/db.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 const pool = mysql.createPool({
   host: 'localhost',
   user: 'root',
-  password: 'AinyTonota4',
+  password: '',
   database: 'asphalt_base',
 });
 
